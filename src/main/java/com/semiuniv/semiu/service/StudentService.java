347ece394b54ba7595ded805d8d5a1c411e2f92b
fine <finelee1119@gmail.com>
2024-05-09 17:03:02 +0900

    #관리자 - 학생 CRUD 1차 완성

---
+++ src/main/java/com/semiuniv/semiu/service/StudentService.java
@@ -0,0 +1,57 @@
+package com.semiuniv.semiu.service;
+
+import com.semiuniv.semiu.dto.StudentDto;
+import com.semiuniv.semiu.entity.Department;
+import com.semiuniv.semiu.entity.Student;
+import com.semiuniv.semiu.repository.DepartmentRepository;
+import com.semiuniv.semiu.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+    private final DepartmentRepository departmentRepository;
+
+    public StudentService(StudentRepository studentRepository, DepartmentRepository departmentRepository) {
+        this.studentRepository = studentRepository;
+        this.departmentRepository = departmentRepository;
+    }
+
+    //등록
+    public void insertStudent(StudentDto dto) {
+        Student student = StudentDto.toStudentEntity(dto);
+        studentRepository.save(student);
+    }
+
+    //조회
+    public List<StudentDto> showAllStudents() {
+        List<StudentDto> studentDtoList = new ArrayList<>();
+        return studentRepository.findAll()
+                .stream()
+                .map(StudentDto::fromStudentEntity)
+                .toList();
+    }
+
+    public StudentDto showOneStudent(Integer id) {
+        return studentRepository.findById(id)
+                .map(StudentDto::fromStudentEntity)
+                .orElse(null);
+    }
+
+    //수정
+    public void updateStudent(StudentDto dto) {
+        Student student = StudentDto.toStudentEntity(dto);
+        studentRepository.save(student);
+    }
+
+    //삭제
+    public void deleteStudent(Integer id) {
+        studentRepository.deleteById(id);
+    }
+
+}
