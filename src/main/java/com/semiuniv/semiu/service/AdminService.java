3d5289e32304c86bb13581baf89b78163e30f7ff
finelee1119 <finelee1119@gmail.com>
2024-05-09 23:38:37 +0900

    #관리자 - 관리자 CRUD 1차 완성

---
+++ src/main/java/com/semiuniv/semiu/service/AdminService.java
@@ -0,0 +1,52 @@
+package com.semiuniv.semiu.service;
+
+import com.semiuniv.semiu.dto.AdminDto;
+import com.semiuniv.semiu.entity.Admin;
+import com.semiuniv.semiu.repository.AdminRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AdminService {
+
+    private final AdminRepository adminRepository;
+
+    public AdminService(AdminRepository adminRepository) {
+        this.adminRepository = adminRepository;
+    }
+
+    //등록
+    public void insertAdmin(AdminDto dto) {
+        Admin admin = AdminDto.toAdminEntity(dto);
+        adminRepository.save(admin);
+    }
+
+    //조회
+    public List<AdminDto> showAllAdmins() {
+        List<AdminDto> adminDtoList = new ArrayList<>();
+        return adminRepository.findAll()
+                .stream()
+                .map(AdminDto::fromAdminEntity)
+                .toList();
+    }
+
+    public AdminDto showOneAdmin(Integer id) {
+        return adminRepository.findById(id)
+                .map(AdminDto::fromAdminEntity)
+                .orElse(null);
+    }
+
+    //수정
+    public void updateAdmin(AdminDto dto) {
+        Admin admin = AdminDto.toAdminEntity(dto);
+        adminRepository.save(admin);
+    }
+
+    //삭제
+    public void deleteAdmin(Integer id) {
+        adminRepository.deleteById(id);
+    }
+
+}
