6189b65b8ae8436fd1f6b67109a8d7f25de63fa5
fine <finelee1119@gmail.com>
2024-05-09 16:46:35 +0900

    #Entity DATE -> LOCALDATE 변경 #ENUM -> CONSTANT폴더로 집합 #STUDENT 초안 삭제 (수정 후 업로드 예정)

--- src/main/java/com/semiuniv/semiu/controller/StudentController.java
+++ src/main/java/com/semiuniv/semiu/controller/StudentController.java
@@ -1,28 +1,17 @@
 package com.semiuniv.semiu.controller;
 
-import com.semiuniv.semiu.entity.Student;
-import com.semiuniv.semiu.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/semi")
 @RequiredArgsConstructor
 public class StudentController {
 
-    private final StudentService studentService;
 
-    @GetMapping("/students")
-    public String list(Model model) {
-        List<Student> students = studentService.findStudents();
-        model.addAttribute("students", students);
-        return "students/studentList";
-    }
 
 
     //수강 신청 페이지 : username로 이동 (조건 : 로그인 후)
