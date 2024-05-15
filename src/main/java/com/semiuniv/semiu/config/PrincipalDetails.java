160b2891a74080cf9eac9ba5994e357df8ed85fb
finelee1119 <finelee1119@gmail.com>
2024-05-15 23:02:54 +0900

    #role 부여 위한 수정 1차

--- src/main/java/com/semiuniv/semiu/config/PrincipalDetails.java
+++ src/main/java/com/semiuniv/semiu/config/PrincipalDetails.java
@@ -2,26 +2,32 @@ package com.semiuniv.semiu.config;
 
 import com.semiuniv.semiu.entity.Users;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class PrincipalDetails implements UserDetails {
-    private Users users;
+    private final Users users;
+
     public PrincipalDetails(Users users){
         this.users=users;
     }
+
     public Users getUsers(){
         return users;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> collect = new ArrayList<>();
-        collect.add(() -> {
-            return users.getRole().toString();});
-        return collect;
+//        Collection<GrantedAuthority> collect = new ArrayList<>();
+//        collect.add(() -> {
+//            return users.getRole().toString();});
+//        return collect;
+
+        return Collections.singletonList(new SimpleGrantedAuthority(users.getRole().name()));
     }
 
     @Override
